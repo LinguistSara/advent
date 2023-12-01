5980e2df90fce458cbc498c776e88e87359e6963
LinguistSara <=>
2023-12-01 19:03:37 +0100

    Day 1, part 1.

--- 2023/Day01.cs
+++ 2023/Day01.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,7 +14,34 @@ public class Day01 : Day
 
     public override object Part1(List<string> input)
     {
-        return 0;
+        var calibrationList = new List<int>();
+        foreach (var row in input)
+        {
+            var numbers = new List<char>();
+            foreach (var c in row)
+            {
+                if (char.IsDigit(c)) 
+                {  
+                    numbers.Add(c);
+                }
+            }
+            var first = numbers[0]; /*det är en char för att det är ett tecken från en sträng*/
+            var last = numbers[^1];
+            //var l = numbers.Last(); samma som den ovan
+            //var number = first.ToString() + last.ToString(); gör samma som den under
+            var num = $"{first}{last}"; //string-interpolering - använda variabler inne i en sträng       
+            var x = int.Parse(num); //gör om num(som är en sträng) till int
+            calibrationList.Add(x);
+        }
+
+        //var summa = calibrationList.Sum();
+        var summa = 0;
+        foreach (var x in calibrationList)
+        {
+            summa += x;
+        }
+
+        return summa;
     }
     public override object Part2(List<string> input)
     {
