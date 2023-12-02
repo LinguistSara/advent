1110142f0608ecc8072b532fe8856e6761371ee8
LinguistSara <=>
2023-12-02 21:15:06 +0100

    Day 2 - complete.

--- 2023/Day02.cs
+++ 2023/Day02.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Advent.y2023;
 
@@ -13,11 +14,54 @@ public class Day02 : Day
 
     public override object Part1(List<string> input)
     {
-        return 0;
+        var sum = 0;
+        foreach (var row in input)
+        {
+            var idAndData = row.Split(":");
+            var id = int.Parse(idAndData[0].Replace("Game ",""));
+            //var id = int.Parse(idAndData[0][5..]); börja på 5 och fortsätt till slutet
+            var red = GetMaxValueForColour("red", idAndData[1]);
+            var green = GetMaxValueForColour("green", idAndData[1]);
+            var blue = GetMaxValueForColour("blue", idAndData[1]);
+            if (red <= 12 && green <= 13 && blue <= 14)
+            {
+                sum += id;
+            }
+
+        }
+        return sum;
     }
+
+    
+
     public override object Part2(List<string> input)
     {
-        return 0;
+        var sum = 0;
+        foreach (var row in input)
+        {
+            var idAndData = row.Split(":");
+            var red = GetMaxValueForColour("red", idAndData[1]);
+            var green = GetMaxValueForColour("green", idAndData[1]);
+            var blue = GetMaxValueForColour("blue", idAndData[1]);
+            sum += red * green * blue;
+        }
+        return sum;
     }
+
+    private int GetMaxValueForColour(string colour, string data)
+    {
+        var c = Regex.Matches(data, @$"\d+ {colour}");
+        var max = 0;
+        foreach (Match item in c)
+        {
+            var value = int.Parse(item.Value.Replace($" {colour}", ""));
+            if (value > max)
+            {
+                max = value;
+            }
+        }
+        return max;
+    }
+
 }
 
